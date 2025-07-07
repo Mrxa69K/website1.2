b83f7d8e1001acdea9ebca754185a3ff5e53a439
Melissa SLIMANI <92652948+Mrxa69K@users.noreply.github.com>
2025-07-07 14:33:32 +0200

    Create favicon.js

---
+++ WEBSITE/final version/js/favicon.js
@@ -0,0 +1,4 @@
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = "/images/logo-paris-love-photography-horizontal.png"; // ou ton chemin exact
+document.head.appendChild(link);
