efe6b18295dcc4e4a39d44984ef822f453da7d16
Melissa SLIMANI <92652948+Mrxa69K@users.noreply.github.com>
2025-07-08 20:10:52 +0200

    Delete WEBSITE/final version directory

--- WEBSITE/final version/js/favicon.js
+++
@@ -1,4 +0,0 @@
-const link = document.createElement("link");
-link.rel = "icon";
-link.href = "/images/logo-paris-love-photography-horizontal.png"; // ou ton chemin exact
-document.head.appendChild(link);
