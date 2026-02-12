8d4b0ddefdcb85867b82af85742038612d6ca406
Melissa SLIMANI <92652948+Mrxa69K@users.noreply.github.com>
2026-02-12 01:28:31 +0100

    Update supabase-config.js

--- WEBSITE/final-version/js/supabase-config.js
+++ WEBSITE/final-version/js/supabase-config.js
@@ -1,5 +1,5 @@
 // Get these from your Supabase project settings
 const SUPABASE_CONFIG = {
     url: 'https://amrzoqmnkfnewujbeqdn.supabase.co',
-    anonKey: 'sb_publishable_H0Wn5I1eqOad7xfx6oM9zQ_CYDSnKXw'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcnpvcW1ua2ZuZXd1amJlcWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4NTA3NjksImV4cCI6MjA4NjQyNjc2OX0.RcAZWKxt4cddA16m6cULPu19HY5jKKv_zNZsDSJ4zkE'
 };
